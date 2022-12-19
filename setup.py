2f6dda712b10f0a7709a6ad9c70a12d8dcd59574
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2022-12-19 13:06:54 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='devialet',
+    packages=['devialet'],
+    version='0.0.1',
+    license='MIT',
+    description='Devialet API',
+    author='fwestenberg',
+    author_email='',
+    url='https://github.com/fwestenberg/devialet',
+    download_url='https://github.com/fwestenberg/devialet/releases/latest',
+    keywords=['Reolink', 'Home-Assistant'],
+    install_requires=[
+        'aiohttp'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
