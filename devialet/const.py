9517bc9efba92c1944b2fd9445815935f53ae037
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2022-12-19 13:24:18 +0100

    Update const.py

--- devialet/const.py
+++ devialet/const.py
@@ -2,10 +2,6 @@
 import logging
 from typing import Final
 
-DOMAIN: Final = "devialet"
-DEFAULT_NAME: Final = "Devialet"
-DEFAULT_SCAN_INTERVAL = 5
-MANUFACTURER: Final = "Devialet"
 LOGGER = logging.getLogger(__package__)
 
 NORMAL_INPUTS = {
@@ -22,13 +18,6 @@ NORMAL_INPUTS = {
     "Phono": "phono",
     "Raat": "raat",
     "Spotify Connect": "spotifyconnect",
-}
-
-SOUND_MODES = {
-    "Custom": "custom",
-    "Flat": "flat",
-    "Night mode": "night mode",
-    "Voice": "voice",
 }
 
 SPEAKER_POSITIONS = {
